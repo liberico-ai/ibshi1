54bbaf9a40213c044e1f0b37d1fe394ff40fda28
Son TT <sontt@ibshi.com>
2026-05-05 17:11:15 +0700

    chore: cleanup temporary scripts

--- scripts/check_categories.ts
+++
@@ -1,8 +0,0 @@
-import prisma from '../src/lib/db'
-
-async function main() {
-  const materials = await prisma.material.groupBy({ by: ['category'], _count: true })
-  console.log(materials)
-}
-
-main().catch(console.error).finally(() => prisma.$disconnect())
